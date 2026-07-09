7bce10266c34a5dade0882e69bc6f7eebabac830
Songyun Bae <songyunbae54@gmail.com>
2026-07-09 11:26:02 +0000

    update

--- Bioinformatics_Stronghold/19.Enumerating_Gene_Orders/solution.py
+++ Bioinformatics_Stronghold/19.Enumerating_Gene_Orders/solution.py
@@ -4,9 +4,20 @@ basket = []
 for i in range(1, n+1):
     basket.append(i)
 
-def permutations(basekt):
-    for i in range(len(basket)):
-        print(' '.join(str(i) for i in basket))
-        print(i)
-    basket = basket[0] + basket[2] + basket[1]
-    print(basket)
+# 가짓수 출력하기
+answer = 1
+for i in range(1, n+1):
+    answer *= i
+print(answer)
+
+# 재귀함수를 활용한 permutation 함수 -> itertools의 permutation 함수도 있음
+def permutations(current, remaining):
+    if not remaining:
+        print(' '.join(str(i) for i in current))
+        return
+    for i in range(len(remaining)):
+        next_current = current + [remaining[i]]
+        next_remaining = remaining[:i] + remaining[i+1:]
+        permutations(next_current, next_remaining)
+
+permutations([], basket)
